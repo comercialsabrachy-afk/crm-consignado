71ae279c8333f9c334d0dd3ee0775c1e83f9692b
sabrachy <comercialsabrachy@gmail.com>
2026-03-08 03:19:01 -0300

    fix allowed hosts

--- crm/settings.py
+++ crm/settings.py
@@ -16,7 +16,13 @@ DEBUG = "RENDER" not in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+import os
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
